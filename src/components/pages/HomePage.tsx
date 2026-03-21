95bb02aec45cfda3411f26261e6d08a749690338
Wix Remote Machine <support@wix.com>
2026-03-21 18:06:02 +0000

    Atualizando a Forma

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -110,7 +110,7 @@ export default function HomePage() {
           <div className="max-w-3xl text-white">
             <div className="space-y-6">
               <div className="flex items-center space-x-2 text-accent">
-                <Star className="w-5 h-5 fill-[#ffac00] stroke-[#ffac00]" />
+                <Star className="w-5 h-5 stroke-[#ffac00] fill-[#ffac00ff]" />
                 <span className="font-paragraph text-lg font-medium text-[#ffac00ff]">Mais de 20 anos de experiência</span>
               </div>
               <h1 className="font-heading text-5xl md:text-7xl font-bold leading-tight">
